5d0e45e0f9995a200a79f365674962ffae8b6f9d
hrs2203 <hrishabh2203@gmail.com>
2022-03-05 20:37:10 +0530

    db working

--- backend_code/index.js
+++ backend_code/index.js
@@ -1,50 +1,49 @@
 // ===== standart http server ======
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+// ======== db instance ============
+var db = {};
+
+function makeVideoId(length = 15) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function addNewVideoLink() {
+  var videoName = makeVideoId()
+  while (db[videoName] !== undefined) { videoName = makeVideoId() }
+  db[videoName] = { "username": new Set(), "connectionList": [], "screenAccess": "" }
+  return videoName
+}
+
 // provide static files
-app.use(express.static('static_build'))
+app.use(express.static('static_build'));
+
+app.use(cors());
+
+app.get("/createVideo", (req, res) => {
+  const newVideoId = addNewVideoLink();
+  return res.status(200).json({ "sid": newVideoId });
+});
+
+app.get("/endVideo", (req, res) => {
+  try {
+    delete db[req.params.sessionId];
+    return res.status(200).json({ "status": true });
+  } catch (_) {
+    return res.status(500).json({ "status": false });
+  }
+});
+
+app.get("/getDb", (req, res) => { return res.status(200).json({ "data": db }) });
+
 app.listen(8000, () => { console.log("server running at port 8000") })
 
 // ======== ws code =========
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-var connectionList = [];
-
-var server = http.createServer(function (request, response) {
-  console.log((new Date()) + ' Received request for ' + request.url);
-  response.writeHead(404);
-  response.end();
-});
-server.listen(8080, function () {
-  console.log((new Date()) + ' Server is listening on port 8080');
-});
-
-wsServer = new WebSocketServer({ httpServer: server });
-
-
-wsServer.on('request', function (request) {
-  try {
-    var connection = request.accept(null, request.origin);
-
-    connectionList.push(connection);
-
-    console.log((new Date()) + ' Connection accepted.');
-
-    connection.on('message', function (message) {
-      if (message.type === 'utf8') {
-        // console.log('Received Message: ' + message.utf8Data);
-        connectionList.filter(item => item !== null).forEach(point => {
-          point.sendUTF(message.utf8Data); // brodcast to every connected port;
-        })
-      }
-    });
-
-    connection.on('close', function (reasonCode, description) {
-      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected for reason code' + reasonCode);
-      connectionList[connectionList.indexOf(connection)] = null; // no more need;
-    });
-
-  } catch (err) { console.log(err) }
-});
-
